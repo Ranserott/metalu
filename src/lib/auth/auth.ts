9142172cff36f11162ed2cc96f685515fd8a8980
ranserot <ranserot3000@gmail.com>
2026-05-29 12:03:37 -0400

    fix(auth): change email field to usuario

--- src/lib/auth/auth.ts
+++ src/lib/auth/auth.ts
@@ -8,16 +8,16 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     Credentials({
       name: "credentials",
       credentials: {
-        email: { label: "Email", type: "email" },
+        username: { label: "Usuario", type: "text" },
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials) {
-        if (!credentials?.email || !credentials?.password) {
+        if (!credentials?.username || !credentials?.password) {
           return null;
         }
 
-        const user = await prisma.user.findUnique({
-          where: { email: credentials.email as string, deletedAt: null },
+        const user = await prisma.user.findFirst({
+          where: { name: credentials.username as string, deletedAt: null },
           include: { roles: { include: { role: true } } },
         });
 
